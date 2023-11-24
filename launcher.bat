9eb96ff1f368d6b6d62123077512ffb1a18201ad
pump25 <95190671+pump25@users.noreply.github.com>
2023-11-24 11:15:01 +0100

    launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,1 +1,3 @@
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --window-position=0,0 --kiosk http://localhost:8000/index2.html  --user-data-dir=c:/monitor1
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --window-position=1920,0 --kiosk http://localhost:8000/ --user-data-dir=c:/monitor2
 node index.js
